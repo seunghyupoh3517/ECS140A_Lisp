85835a226582c044c4104173eabfc696f6b2a0e9
Seunghyup Alex Oh <seunghyupoh3517@gmail.com>
2021-02-13 17:54:11 -0800

    Delete hw3-handout directory

--- hw3-handout/nfa/nfa.lisp
+++
@@ -1,7 +0,0 @@
-;; You may define helper functions here
-
-(defun reachable (transition start final input)
-    ;; TODO: Incomplete function
-    ;; The next line should not be in your solution.
-    (list 'incomplete)
-)
