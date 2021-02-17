e6e334b783e5a79e164a2319e08812a7ec3917b1
Seunghyup Alex Oh <seunghyupoh3517@gmail.com>
2021-02-16 19:20:24 -0800

    Add files via upload

--- nfa/nfa.lisp
+++ nfa/nfa.lisp
@@ -1,7 +1,51 @@
 ;; You may define helper functions here
-
-(defun reachable (transition start final input)
+(defun reachable (transition start final input) 
     ;; TODO: Incomplete function
     ;; The next line should not be in your solution.
-    (list 'incomplete)
+    (cond ((null input)
+        ;; when input is null, check whether the start and final is equal
+            (funcall (lambda (s1 s2) (eq s1 s2)) start final )) 
+
+        ;; when input is not null, proceed to the next state with car(input)
+        ;; then recursive call with next state and cdr(input)
+        ;; Multiple possible next states can be called interatively 
+        ;; transition f returns list of possible states
+          ((not(null input))
+            (setq next (funcall transition start (car input))) ;; list of next states 
+        ;; Prolbem#1 - how to iteratively call the reachable() with different next starting states
+            (reach_helper1 transition next final (cdr input)))
+    ) 
+) 
+
+;; Send the list of next states which is generated with start state and input
+(defun reach_helper1 (transition nextlist final input)
+    (cond ((null input)
+        ;; Problem#2 - comparing the element of list and atom is not possible, mapcar takes arg list only
+        ;; i.e. nextlist (0 1 3) -> truelist (nil nil t) when final is 3
+        ;; if any of the possible next state is final state, reachable
+        ;; (reach_helper2 truelist))) 
+            (reach_helper2 (setq truelist (mapcar #'(lambda (s1) (eq s1 final)) nextlist))))
+        
+          ((not(null input))
+            (or (reachable transition (car nextlist) final input)
+            (reach_helper3 transition (cdr nextlist) final input))
+          )
+    )
+)
+
+;; truelist from reach_helper1 can be checked if it reaches the final state here
+(defun reach_helper2 (start)
+    (if (not (null start))
+            (cond 
+                ((eq (car start) t) (eval t))
+                ((eq (car start) nil) (reach_helper2 (cdr start)))
+            )
+    )
+)
+
+;; In order to iteratively go all the state in the list of next states
+(defun reach_helper3 (transition nextlist final input)
+    (if (not (null (car nextlist))) 
+        (or (reachable transition (car nextlist) final input)
+        (reach_helper3 transition (cdr nextlist) final input)))
 )
--- nfa/nfa_test.lisp
+++ nfa/nfa_test.lisp
@@ -15,6 +15,7 @@
 (defun fooTransitions (state input)
     ;;  * 0 -a-> 1
 	;;  * 0 -a-> 2
+    ;;  * 0 -b-> 2
 	;;  * 1 -b-> 3
 	;;  * 2 -c-> 3
 
@@ -52,7 +53,28 @@
     ((and (eq state 1) (eq input 'B ))  (list 0))
     (t (list nil))
     )
+)
 
+(defun manualTransitions (state input)
+	;;  * 0 -a-> 1
+    ;;  * 0 -a-> 2
+    ;;  * 0 -a-> 3
+    ;;  * 0 -a-> 4
+	;;  * 1 -a-> 0
+	;;  * 2 -b-> 0
+    ;;  * 2 -b-> 3
+    ;;  * 3 -c-> 0
+    ;;  * 3 -c-> 4
+    ;;  * 4 -d-> 0
+    ;;  * 4 -d-> 1
+  (cond
+    ((and (eq state 0) (eq input 'A ))  (list 1 2 3 4))
+    ((and (eq state 1) (eq input 'A ))  (list 0))
+    ((and (eq state 2) (eq input 'B ))  (list 0 3))
+    ((and (eq state 3) (eq input 'C ))  (list 0 4))
+    ((and (eq state 4) (eq input 'D ))  (list 0 1))
+    (t (list nil))
+    )
 )
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; reachable test definitions ;;;
@@ -64,6 +86,7 @@
     (assert-equal T (reachable 'fooTransitions 0 3 '(A B)))
     (assert-equal NIL (reachable 'fooTransitions 0 3 '(A A A)))
     (assert-equal T (reachable 'fooTransitions 0 3 '(A C)))
+    (assert-equal T (reachable 'fooTransitions 0 2 '(B)))
 
     (assert-equal T (reachable 'expTransitions 0 0 NIL))
     (assert-equal NIL (reachable 'expTransitions 0 2 '(B B)))
@@ -76,6 +99,13 @@
     (assert-equal T (reachable 'langTransitions 0 0 '(A A A)))
     (assert-equal NIL (reachable 'langTransitions 0 1 '(A B B A)))
     (assert-equal T (reachable 'langTransitions 0 1 '(B A B A B)))
+
+    (assert-equal T (reachable 'manualTransitions 0 1 '(A A A B A C A D)))
+    (assert-equal T (reachable 'manualTransitions 0 0 '(A A A B A C A D)))
+    (assert-equal NIL (reachable 'manualTransitions 0 4 '(A B C D)))
+    (assert-equal T (reachable 'manualTransitions 0 0 '(A A)))
+    (assert-equal T (reachable 'manualTransitions 1 4 '(A A)))
+    (assert-equal T (reachable 'manualTransitions 3 4 '(C)))
 )
 ;;;;;;;;;;;;;;;;;
 ;;; run tests ;;;
