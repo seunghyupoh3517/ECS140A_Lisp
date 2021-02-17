cc1362a2c0ef6a82cd8994148095521bf805369f
Jerry Zhu <zhurq0321@gmail.com>
2021-02-17 09:58:39 +0800

    add explain

--- match/match.lisp
+++ match/match.lisp
@@ -1,7 +1,5 @@
 ; You may define helper functions here
 
-
-
 ;; (defun matchsame (ls1 ls2)
 ;;   (equal ls1 ls2) 
 ;; )
@@ -22,7 +20,7 @@
   ;; TODO: incomplete function. 
   ;; The next line should not be in your solution.
   (cond
-    ;(equal (pattern assertion null) t)
+    ; (equal (pattern assertion null) t)
     ((and (null pattern) (null assertion) T)) ;check if empty
 
     ((equal pattern assertion) T) ; all same return T and end
@@ -32,15 +30,19 @@
 
     ((equal (car pattern) '?) (match(cdr pattern) (cdr assertion))) ;detect ?
 
-    ((and (equal (car pattern) '!) (equal (cdr pattern) nil) (not (equal (cdr assertion) nil))T)) ;see ! at the end of pattern
+    ((and (equal (car pattern) '!) (equal (cdr pattern) nil) (not (equal (cdr assertion) nil))T)) 
+    ; see ! at the end of pattern
     ((equal (car pattern) '!) (or (match pattern (cdr assertion)) (match(cdr pattern) (cdr assertion))) )
-    
+    ; one "or" fail, could go to another. first "or" could find if there is anything after ! that match
+    ; second "or" if there is just one ! means one letter  
+
     ; find !, find the next matching letter
     ; 1: if the ! is the last element in pattern, then return true
     ; 2: if the ! is not the last element, find the letter that match with the letter after ! or ? after cdr assertion 
     
 
     ((equal (car pattern) (car assertion))  (match(cdr pattern) (cdr assertion)) );rec iterate through
-    ;first letter same, moving forward
+    ; if we miss all matches from cond above but still match the first letter, moving forward
+    ; first letter same, moving forward
   )
 )
