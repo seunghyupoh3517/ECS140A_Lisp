971346f35f2184367f6d53c6bf0eb754d7500f10
WeiyiZ-008 <59705192+WeiyiZ-008@users.noreply.github.com>
2021-02-19 12:18:40 -0800

    Build the frame by using RDP

--- sexpr/parser.go
+++ sexpr/parser.go
@@ -19,6 +19,119 @@ type Parser interface {
 	Parse(string) (*SExpr, error)
 }
 
-func NewParser() Parser {
-	panic("TODO: implement NewParser")
+// Grammar struct to implements the Parser interface
+type Grammar struct {
+	grammar map[*SExpr][]*SExpr    // TODO: need to change the attributes inside struct
 }
+
+// TODO: need to change the Gramar constructor
+func NewParser() Parser {
+	var parseGrammar Parser = Grammar{make(map[*SExpr][]*SExpr)}
+	return parseGrammar
+}
+
+
+// implements the Parser interface
+func (g Grammar) Parse(str string) (*SExpr, error) {
+
+	// Tokennize the input string
+	lex := newLexer(str)
+	var	tokenList []*Token
+	for {
+		token, err := lex.next()
+
+		// validating the given string, return error if can't parse to token
+		if err == ErrLexer {
+			return nil, ErrParser
+		} else {
+			if token.typ == tokenEOF {
+				tokenList = append(tokenList, token)
+				break
+			}
+			tokenList = append(tokenList, token)
+		}
+	}
+	// pointer point to the current token in the list
+	var tokenInd = 0
+
+
+
+}
+
+// create the SExpr for the matched terminal
+func matchTerminal(token *Token) (*SExpr, error) {
+	// Three type of creation
+
+
+	// 1. create the nil s-expression
+
+
+	// 2. create the atom s-expression
+
+
+	// 3. create the list s-expression
+
+
+
+}
+
+
+// grammar after left factoring
+// <sexpr>       ::= NUM | SYM | ( <New1> | QUOTE <sexpr>
+// <proper_list> ::= <sexpr> <proper_list> | \epsilon
+// <New1>        ::= <proper_list> <New2>
+// <New2>        ::= <sexpr> DOT <sexpr> ) | )
+
+// procedure for <sexpr>
+func parse_sexpr() {
+	tokenTyp := tokenList[tokenInd].typ
+	if tokenTyp == tokenNumber || tokenTyp == tokenSymbol {
+		// match the terminal
+		// increment the tokenInd
+	} else if tokenTyp == tokenLpar {
+		// <sexpr> -> ( <New1>
+
+		// match the (
+		parse_New1()
+	} else if tokenTyp == tokenQuote {
+		// <sexpr> -> QUOTE <sexpr>
+
+		// match the QUOTE
+		parse_sexpr()
+	}
+}
+
+// procedure for <proper_list>
+func parse_proper_list() {
+	tokenTyp := tokenList[tokenInd].typ
+	if tokenTyp == tokenEOF {
+		// match \epsilon
+	} else {
+		// <proper_list> -> <sexpr> <proper_list>
+		parse_sexpr()
+		parse_proper_list()
+	}
+}
+
+// procedure for <New1>
+func parse_New1() {
+	parse_proper_list()
+	parse_New2()
+}
+
+// procedure for <New2>
+func parse_New2() {
+	tokenTyp := tokenList[tokenInd].typ
+	if tokenTyp == tokenRpar {
+		// <New2> -> )
+
+		// match the )
+	} else {
+		// <New2> -> <sexpr> DOT <sexpr> )
+		parse_sexpr()
+		// TODO: check match DOT 
+		parse_sexpr()
+		// TODO: check mathch )
+	}
+}
+
