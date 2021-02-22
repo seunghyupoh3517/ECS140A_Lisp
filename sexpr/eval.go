e6d039e2bf8f85e48ea650045b2c5f7c894ef1a4
Jerry Zhu <zhurq0321@gmail.com>
2021-02-22 10:29:18 +0800

    init

--- sexpr/eval.go
+++ sexpr/eval.go
@@ -9,8 +9,50 @@ import (
 // an invalid token.
 // See also https://golang.org/pkg/errors/#New
 // and // https://golang.org/pkg/builtin/#error
+
 var ErrEval = errors.New("eval error")
 
 func (expr *SExpr) Eval() (*SExpr, error) {
-	panic("TODO: implement Eval")
+	//panic("TODO: implement Eval")
+	
+	
+	expr.mkAtom("1")
+
+	expr.isNil()
+	if atom 
+	
+	return nil
 }
+
+func Car(e *SExpr) *SExpr {
+	if e.atom == nil || isAtom == nil{
+		return nil
+	}
+	else {
+		return e.car
+	}
+}
+
+func Car(expr *SExpr) *Expr{
+	return e.car
+}
+
+func (expr *SExpr) Check_atom() *Expr{
+
+	return True
+}
+
+func (expr *SExpr) Check_atom() *Expr{
+
+	return True
+}
+
+func (expr *SExpr) Check_Num() *Expr{
+
+	return True
+}
+
+func (expr *SExpr) Check_add() *Expr{
+
+}
+// func ()
