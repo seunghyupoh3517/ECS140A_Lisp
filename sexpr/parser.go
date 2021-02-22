9b054549e6af14ddf0c44708db528d680780126a
Drao <hdjchen@ucdavis.edu>
2021-02-21 20:26:57 -0800

    passed proper_list

--- sexpr/parser.go
+++ sexpr/parser.go
@@ -1,6 +1,8 @@
 package sexpr
 
-import "errors"
+import (
+	"errors"
+)
 
 // ErrParser is the error value returned by the Parser if the string is not a
 // valid term.
@@ -30,13 +32,15 @@ func NewParser() Parser {
 	return parseGrammar
 }
 
+var tokenInd = 0
+var tokenList []*token
 
 // implements the Parser interface
 func (g Grammar) Parse(str string) (*SExpr, error) {
 
 	// Tokennize the input string
 	lex := newLexer(str)
-	var	tokenList []*Token
+	// var	tokenList []*token
 	for {
 		token, err := lex.next()
 
@@ -51,37 +55,41 @@ func (g Grammar) Parse(str string) (*SExpr, error) {
 		}
 	}
 	// pointer point to the current token in the list
-	var tokenInd = 0
+	// var tokenInd = 0
 
-
+	// var se = &SExpr{}
+	var se, error = parse_sexpr()
+	tokenInd = 0
+	tokenList = nil
+	return se, error
 
 }
 
 // create the SExpr for the matched terminal
-func matchTerminal(token *Token) (*SExpr, error) {
-	// Three types of creation
+// func matchTerminal(token *Token) (*SExpr, error) {
+// 	// Three types of creation
 
 
-	// 1. create the nil s-expression
-	se := mkNil()
-	return se, nil
+// 	// 1. create the nil s-expression
+// 	se := mkNil()
+// 	return se, nil
 
 
-	// 2. create the atom s-expression
-	if token.typ == tokenNumber {
-		se := mkNumber(token.num)
-		return se, nil
-	} else if token.typ == tokenSymbol {
-		se := mkAtom(mkSymbol(token.literal))
-		return se, nil
-	}
+// 	// 2. create the atom s-expression
+// 	if token.typ == tokenNumber {
+// 		se := mkNumber(token.num)
+// 		return se, nil
+// 	} else if token.typ == tokenSymbol {
+// 		se := mkAtom(mkSymbol(token.literal))
+// 		return se, nil
+// 	}
 
 
-	// 3. create the list s-expression
+// 	// 3. create the list s-expression
 
 
 
-}
+// }
 
 
 // grammar after left factoring
@@ -91,9 +99,13 @@ func matchTerminal(token *Token) (*SExpr, error) {
 // <New2>        ::= <sexpr> DOT <sexpr> ) | )
 
 // procedure for <sexpr>
-func parse_sexpr() *SExpr {
+func parse_sexpr() (*SExpr, error) {
+
 	token := tokenList[tokenInd]
-	switch tokenTyp := token.typ {
+
+	tokenTyp := token.typ
+
+	switch tokenTyp {
 
 	case tokenNumber:
 		se := mkNumber(token.num)
@@ -108,14 +120,17 @@ func parse_sexpr() *SExpr {
 	case tokenLpar:
 		// <sexpr> -> ( <New1>
 		// match the (
+		tokenInd += 1
 		se := parse_New1()
 		return se,  nil
 
 	case tokenQuote:
 		// <sexpr> -> QUOTE <sexpr>
 		// match the QUOTE
-		se := parse_sexpr()
-		return se, nil
+		tokenInd += 1
+		// se, error := parse_sexpr()
+		// return se, nil
+		return parse_sexpr()
 		
 	default:
 		return nil, ErrParser
@@ -124,13 +139,13 @@ func parse_sexpr() *SExpr {
 
 // procedure for <proper_list>
 func parse_proper_list() *SExpr {
-	tokenTyp := tokenList[tokenInd].typ
-	if tokenTyp == tokenEOF {
-		// match \epsilon
+	// tokenTyp := tokenList[tokenInd].typ
+	se, error := parse_sexpr()
+	
+	if error != nil {
 		return mkNil()
 	} else {
-		// <proper_list> -> <sexpr> <proper_list>
-		car := parse_sexpr()
+		car := se
 		cdr := parse_proper_list()
 		return mkConsCell(car, cdr)
 	}
@@ -138,9 +153,10 @@ func parse_proper_list() *SExpr {
 
 // procedure for <New1>
 func parse_New1() *SExpr {
-	car := parse_proper_list()
-	cdr := parse_New2()
-	return mkConsCell(car, cdr)
+	// Remember to cover New2
+	se := parse_proper_list()
+	parse_New2()
+	return se
 }
 
 // procedure for <New2>
@@ -149,6 +165,7 @@ func parse_New2() *SExpr {
 	if tokenTyp == tokenRpar {
 		// <New2> -> )
 		// match the )
+		tokenInd += 1
 		return mkNil()
 	} else {
 		// <New2> -> <sexpr> DOT <sexpr> )
@@ -156,6 +173,8 @@ func parse_New2() *SExpr {
 		// TODO: check match DOT 
 		parse_sexpr()
 		// TODO: check mathch )
+		// Double Check!!
+		return nil
 	}
 }
 
